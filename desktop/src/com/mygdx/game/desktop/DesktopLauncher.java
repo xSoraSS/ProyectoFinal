8abfc2fb44e45c738267276e7c19994045e9f496
dam2a <amateu@elpuig.xeill.net>
2020-02-04 20:36:29 +0100

    SAD

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.FightersGame;
-import com.mygdx.game.MainGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
